831cc52b28ff11a9fc1e13ab9c5df54cd95c4809
vs <valera.sheva@gmail.com>
2021-08-29 17:24:56 +0300

    Task if-alse, for, while,do-while

--- js/script.js
+++ js/script.js
@@ -1,56 +1,90 @@
-
-'use strict';
-
-const numberOfFilms = prompt("How many films did you watch ?","");
-
-
-const personalMovieDB= {
-    count:numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}
-
-const lastFilm = prompt("Last film you watched?","");
-const score = prompt("Your score?","");
-const c = prompt("Last film you watched?","");
-const d = prompt("Your score?","");
-
-personalMovieDB.movies[lastFilm] = score;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
-
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате:
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+
+"use strict";
+
+const numberOfFilms = +prompt("How many films did you watch ?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Last film you watched?", "");
+  let b = prompt("Your score?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("ERROR");
+    i--;
+  }
+}
+
+
+// let i=0;
+
+// while (i<2) { 
+//     let a = prompt("Last film you watched?", "");
+//     let b = prompt("Your score?", "");
+//     i++;
+  
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//       personalMovieDB.movies[a] = b;
+//       console.log("done");
+//     } else {
+//       console.log("ERROR");
+//       i--;
+//     }
+//   }
+
+
+// let i = 0
+
+// do { {
+//     let a = prompt("Last film you watched?", "");
+//     let b = prompt("Your score?", "");
+//     i++
+  
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//       personalMovieDB.movies[a] = b;
+//       console.log("done");
+//     } else {
+//       console.log("ERROR");
+//       i--;
+//     }
+//   }  
+
+// } while ( i < 2);
 
 
 
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
-
-
-
-
-
+console.log(personalMovieDB);
